d08c2c99de4dfc026f4760b90b52809be28f64a3
Laurens Deprost <ldeprost@gmail.com>
2019-02-13 22:47:49 +0100

    feat: Add basic navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./Nav";
+import NewQuestion from "./NewQuestion";
+import Leaderboard from "./Leaderboard";
 
 class App extends Component {
   render() {
-    return <div>react-redux-polls</div>;
+    return (
+      <Router>
+        <div className="App">
+          <Nav />
+          {/* <Fragment> */}
+          <Route exact path="/" render={() => <div>Home</div>} />
+          <Route exact path="/new" component={NewQuestion} />
+          <Route exact path="/leaderboard" component={Leaderboard} />
+          <Route exact path="/questions/:question_id" component={NewQuestion} />
+          {/* </Fragment> */}
+        </div>
+      </Router>
+    );
   }
 }
 
