6609390090523a6f0e04e7b12614f96239cc86e3
Laurens Deprost <ldeprost@gmail.com>
2019-02-13 22:47:23 +0100

    feat: Add basic components for different views

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Leaderboard extends Component {
+  render() {
+    return <div>Leaderboard</div>;
+  }
+}
+
+export default Leaderboard;
---
+++ src/components/NewQuestion.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class NewQuestion extends Component {
+  render() {
+    return <div>New Question</div>;
+  }
+}
+
+export default NewQuestion;
