e975e9c4feb0c846d1be885822bbbb15c873e9c6
Laurens Deprost <ldeprost@gmail.com>
2019-02-25 11:24:42 +0100

    refactor: Convert QuestionPreview to stateless functional component

--- src/components/QuestionPreview.js
+++ src/components/QuestionPreview.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import {
   Card,
@@ -10,54 +10,51 @@ import {
   Button
 } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-
 import { FaEdit, FaPoll } from "react-icons/fa";
 
-class QuestionPreview extends Component {
-  render() {
-    const { question, author, wasAnswered } = this.props;
-    const questionUri = `/questions/${question.id}`;
+const QuestionPreview = (props) => {
+  const { question, author, wasAnswered } = props;
+  const questionUri = `/questions/${question.id}`;
 
-    return (
-      <Card className="question">
-        <Card.Body>
-          <Fragment>
-            <Card.Header as="h5">
-              {author.name} {wasAnswered ? "asked:" : "asks:"}
-            </Card.Header>
-            <Row>
-              <Col xs={3}>
-                <Image
-                  roundedCircle
-                  className="avatar question"
-                  src={author.avatarURL}
-                />
-              </Col>
-              <Col>
-                <span>Would you rather...</span>
-              </Col>
-            </Row>
-          </Fragment>
-
-          <Row className="justify-content-md-center">
-            <ListGroup>
-              <ListGroupItem>{question.optionOne.text}</ListGroupItem>
-              <ListGroupItem>{question.optionTwo.text}</ListGroupItem>
-            </ListGroup>
+  return (
+    <Card className="question">
+      <Card.Body>
+        <Fragment>
+          <Card.Header as="h5">
+            {author.name} {wasAnswered ? "asked:" : "asks:"}
+          </Card.Header>
+          <Row>
+            <Col xs={3}>
+              <Image
+                roundedCircle
+                className="avatar question"
+                src={author.avatarURL}
+              />
+            </Col>
+            <Col>
+              <span>Would you rather...</span>
+            </Col>
           </Row>
-        </Card.Body>
-        <Card.Footer className="text-center">
-          <LinkContainer to={questionUri}>
-            <Button variant="outline-dark">
-              {wasAnswered ? <FaPoll /> : <FaEdit />} 
-              {wasAnswered ? "View Results" : "Answer Poll"}
-            </Button>
-          </LinkContainer>
-        </Card.Footer>
-      </Card>
-    );
-  }
-}
+        </Fragment>
+
+        <Row className="justify-content-md-center">
+          <ListGroup>
+            <ListGroupItem>{question.optionOne.text}</ListGroupItem>
+            <ListGroupItem>{question.optionTwo.text}</ListGroupItem>
+          </ListGroup>
+        </Row>
+      </Card.Body>
+      <Card.Footer className="text-center">
+        <LinkContainer to={questionUri}>
+          <Button variant="outline-dark">
+            {wasAnswered ? <FaPoll /> : <FaEdit />} 
+            {wasAnswered ? "View Results" : "Answer Poll"}
+          </Button>
+        </LinkContainer>
+      </Card.Footer>
+    </Card>
+  );
+};
 
 function mapStateToProps({ users }, props) {
   const questionAuthor = props.question.author;
